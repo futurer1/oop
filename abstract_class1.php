d34b2796ca58acb8df14fd8437b717d7900f8a5c
Michail <futurer@inbox.ru>
2017-05-11 21:54:20 +0400

    Count of arguments in abstract method

---
+++ abstract_class1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Количество аргументов может не совпадать только при задании значений по умолчанию</title>
+</head>
+<body><?php
+abstract class TestClass2
+{
+    abstract public function testMethod();
+}
+class TestClass3 extends TestClass2
+{
+    public function testMethod($var1="значение по умолчанию", $var2=array()) {  //ошибки не будет, потому что мы задали
+                                                                                //значения по умолчанию
+    }
+}
+?>
