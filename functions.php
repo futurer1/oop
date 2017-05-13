824f4f15a93ca2b6c2f04b0f661f112a184c27ff
Michail <futurer@inbox.ru>
2017-05-14 00:10:38 +0400

    Use &$ and $

--- functions.php
+++ functions.php
@@ -3,16 +3,17 @@
     <title>Использование знака &</title>
 </head>
 <body><?php
-echo "Пример 1<br />";
 $var=0;
 $var1=0;
 function test_func(&$name_var, $name_var1){     //аргумент функции со знаком & и без знака &
+                                                //Использование & создает ссылку на переменную
+                                                //Без символа & создается новая локальная переменная внутри функции
     $name_var++;
     $name_var1++;
     return "name_var=".$name_var."; name_var1=".$name_var1;
 }
-echo test_func($var, $var1). "  (var=" .$var. "; var1=" .$var1. ")<br />";
-echo test_func($var, $var1). "  (var=" .$var. "; var1=" .$var1. ")<br />";
-echo test_func($var, $var1). "  (var=" .$var. "; var1=" .$var1. ")<br />";
+echo test_func($var, $var1). "  (var=" .$var. "; var1=" .$var1. ")<br />";  //Выведет: name_var=1; name_var1=1 (var=1; var1=0)
+echo test_func($var, $var1). "  (var=" .$var. "; var1=" .$var1. ")<br />";  //Выведет: name_var=2; name_var1=1 (var=2; var1=0)
+echo test_func($var, $var1). "  (var=" .$var. "; var1=" .$var1. ")<br />";  //Выведет: name_var=3; name_var1=1 (var=3; var1=0)
 ?></body>
 </html>
