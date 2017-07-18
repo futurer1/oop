506c9e053756ba1338e7c8883e838c88edb1488c
Michail <futurer@inbox.ru>
2017-07-18 12:08:07 +0300

    Update exception.php

--- exception.php
+++ exception.php
@@ -14,9 +14,29 @@ try {
     if($b == 0) throw new Exception("Деление на 0!");
     echo $a/$b;
 }
-catch(Exception $e){
+catch (Exception $e) {
     echo "Произошла ошибка - ",
     $e->getMessage(),     // Выводит сообщение  " в строке ",
     $e->getLine(),        // Выводит номер строки " файла ",
     $e->getFile();        // Выводит имя файла
+}
+
+
+//Пример 2. Расширение класса Exception и создание собственного исключения
+сlass MathException extends Exception {
+    function __construct($message) {
+        parent::__construct($message);
+    }
+}
+try {
+    $a = 1;
+    $b = 0;
+    // MathException - имя класса для создания собственного исключения
+    if ($b == 0) throw new MathException("Деление на 0!");
+    echo $a / $b;
+} catch (MathException $e) {
+    echo "Произошла математическая ошибка ",
+    $e->getMessage(),
+    " в строке ", $e->getLine(),
+    " файла ", $e->getFile();
 }
